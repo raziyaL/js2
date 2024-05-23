9dc4d3c3e4deaf6e34cde4ffbeb3465f3301c808
raziyaL <166826366+raziyaL@users.noreply.github.com>
2024-05-23 15:11:44 +0600

    Add files via upload

--- dz/app.js
+++ dz/app.js
@@ -1,31 +1,11 @@
-const btn = document.querySelector('#getData');
-const container = document.querySelector('.class')
-btn.onclick=()=>{
-    const request = new XMLHttpRequest();
-    request.open('GET','person.json');
-    request.setRequestHeader('Content-type','application/json');
-    request.send()
-    request.onload=()=>{
-        const data = JSON.parse(request.response)
-        data.forEach(item=>{
-            const div= document.createElement('div');
-            div.innerHTML=`
-                <img src="${item.person_photo}" alt="photo">
-                <p>name: ${item.name}</p>
-                <p>age: ${item.age}</p>
-            `
-            container.appendChild(div);
-        })
-    }
-}
-
-    const request = new XMLHttpRequest()
-    request.open('GET','free.json')
-    request.setRequestHeader('Content-type', 'application/json')
-    request.send()
-    request.onload = ()=> {
-        const data = JSON.parse(request.response)
-        console.log(data)
-
-    }
-
+ 
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(data=>{
+    data.forEach((post) => {
+    console.log('userId: '+ post.userId)
+        console.log('id: ' + post.id)
+        console.log('title: ' + post.title)
+        console.log( 'body: ' + post.body)
+})
+})
